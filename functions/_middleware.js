fc64c0a687bb6c723e5b75acfdc90f9ba697b3fe
Sebastian LaRosa <dlarosa92@gmail.com>
2025-09-19 13:37:31 -0400

    qa: gate middleware by ENVIRONMENT=qa

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,13 +1,12 @@
-export async function onRequest({ next }) {
+export async function onRequest({ next, env }) {
+  // Only run this logic in QA
+  if (env.ENVIRONMENT !== 'qa') return next();
+
   const res = await next();
-
   const ct = res.headers.get("content-type") || "";
-  if (!ct.includes("text/html")) {
-    // Non-HTML responses untouched
-    return res;
-  }
+  if (!ct.includes("text/html")) return res;
 
-  // Clone headers then set/override
+  // Clone headers, then override
   const h = new Headers(res.headers);
   h.set("x-qa-mw", "hit");
   h.set("x-robots-tag", "noindex, nofollow");
@@ -16,14 +15,10 @@ export async function onRequest({ next }) {
   h.set("referrer-policy", "no-referrer");
   h.set("permissions-policy", "camera=(), microphone=(), geolocation=()");
 
-  // Make QA HTML strictly non-cacheable (CDN + browser)
+  // non-cacheable in QA
   h.set("cache-control", "no-store, no-cache, must-revalidate");
   h.set("pragma", "no-cache");
   h.set("expires", "0");
 
-  return new Response(res.body, {
-    status: res.status,
-    statusText: res.statusText,
-    headers: h,
-  });
+  return new Response(res.body, { status: res.status, statusText: res.statusText, headers: h });
 }
