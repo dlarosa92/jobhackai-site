ac32a3b1b04a3f81245d3da53cec93b33473c019
Sebastian LaRosa <dlarosa92@gmail.com>
2025-09-19 12:14:09 -0400

    chore: remove kv-test route (QA)

--- functions/kv-test.js
+++
@@ -1,8 +0,0 @@
-export const onRequestGet = async ({ env }) => {
-  const key = "hello";
-  await env.KV.put(key, "world", { expirationTtl: 60 });
-  const value = await env.KV.get(key);
-  return new Response(JSON.stringify({ ok: true, value }), {
-    headers: { "content-type": "application/json" },
-  });
-};
