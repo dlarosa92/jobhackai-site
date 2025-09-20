ba7785cd845e54ae0f1e268a2e7c1610f60cedc5
Sebastian LaRosa <dlarosa92@gmail.com>
2025-09-20 00:01:23 -0400

    feat(qa): add FRONTEND_URL check to env-test function

--- functions/env-test.js
+++ functions/env-test.js
@@ -2,6 +2,7 @@ export async function onRequest({ env }) {
   const hasFirebaseApiKey = !!env.FIREBASE_API_KEY;
   const hasStripePublishableKey = !!env.STRIPE_PUBLISHABLE_KEY;
   const hasStripeSecretKey = !!env.STRIPE_SECRET_KEY;
+  const hasFrontendUrl = !!env.FRONTEND_URL;
 
   return new Response(
     JSON.stringify({
@@ -10,6 +11,8 @@ export async function onRequest({ env }) {
       hasFirebaseApiKey,
       hasStripePublishableKey,
       hasStripeSecretKey,
+      hasFrontendUrl,
+      frontendUrl: env.FRONTEND_URL || 'NOT_SET',
       timestamp: new Date().toISOString(),
     }),
     { headers: { 'Content-Type': 'application/json' } }
